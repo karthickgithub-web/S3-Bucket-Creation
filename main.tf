e2bd574461848f6b2ec0edcd3d4921bfb0ad1b38
karthickgithub-web <110174311+karthickgithub-web@users.noreply.github.com>
2024-12-24 20:39:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "Mybucket" {
-  bucket = "karthickjishnu24122024"
+resource "aws_s3_bucket" "Mybucket01" {
+  bucket = "karthickjishnu2412202401"
 
   tags = {
     Name        = "karthickjishnu24122024"
